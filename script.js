53cae256493bced610cbd783e505cb91360308e6
Sakshi Chede <78843328+Sakiii8499@users.noreply.github.com>
2021-09-19 22:48:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//selecting all required elements
+const selectBox = document.querySelector(".select-box"),
+    selectBtnX = selectBox.querySelector(".options .playerX"),
+    selectBtnO = selectBox.querySelector(".options .playerO"),
+    playBoard = document.querySelector(".play-board"),
+    players = document.querySelector(".players")
+
+selectBtnX.onclick = () => {
+    selectBox.classList.add("hide"); //hide select box
+    playBoard.classList.add("show"); //show the playboard section
+}
+
+selectBtnO.onclick = () => {
+    selectBox.classList.add("hide"); //hide select box
+    playBoard.classList.add("show"); //show the playboard section
+    players.setAttribute("class", "players active player"); //set class attribute in players with players active player values
+}
